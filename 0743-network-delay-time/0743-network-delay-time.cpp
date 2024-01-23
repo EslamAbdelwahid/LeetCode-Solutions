7d0b6df4de12ca609abe22055af58c92a8e3bce9
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-01-23 22:01:29 +0200

    Time: 92 ms (72.28%), Space: 40 MB (94.71%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<int>>adjMatrix(n,vector<int>(n,1e9));
-        for(auto &i : times){
-            int u = i[0] - 1, v = i[1] - 1, w = i[2];
-            adjMatrix[u][v] = min(adjMatrix[u][v], w);
-        }
-        for(int i = 0;i < n;++i) adjMatrix[i][i] = 0;
-        for(int k = 0;k < n;++k){
-            for(int i = 0;i < n;++i){
-                for(int j = 0;j < n;++j){
-                    if(adjMatrix[i][k] == 1e9 || adjMatrix[k][j] == 1e9) continue;
-                    int relax = adjMatrix[i][k] + adjMatrix[k][j];
-                    adjMatrix[i][j] = min(adjMatrix[i][j], relax);
+        vector<int>dist(n, 1e9);
+        dist[k - 1] = 0;
+        for(int i = 0;i < n - 1;++i){
+            for(auto &edge : times){
+                int u = edge[0]-1, v = edge[1]-1, cost = edge[2];
+                if(dist[v] > dist[u] + cost){
+                    dist[v] = dist[u] + cost;
                 }
             }
         }
         int ans = 0;
         for(int i = 0;i < n;++i){
-            if(adjMatrix[k - 1][i] == 1e9) return -1;
-            ans = max(ans, adjMatrix[k - 1][i]);
+            if(dist[i] == 1e9) return -1;
+            ans = max(ans, dist[i]);
         }
         return ans;
     }
