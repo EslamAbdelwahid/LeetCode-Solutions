0a452e1e0008463111d0cd7f5c50325fa05c5fa8
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-03-05 23:16:28 +0200

    Time: 250 ms (40.59%), Space: 60.5 MB (13.80%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,15 @@
 class Solution {
+    map < pair<int,int>, int > dp;
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         return solve(0, nums, target);
     }
     int solve(int i, vector<int>&nums, int tar){
         if(i == nums.size()) return tar == 0;
+        if(dp.find({i, tar}) != dp.end()) return dp[{i, tar}];
         int ret = 0;
         ret += solve(i + 1, nums, tar - nums[i]);
         ret += solve(i + 1, nums, tar + nums[i]);
-        return ret;
+        return  dp[{i, tar}] = ret;
     }
 };
