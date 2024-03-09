fc05f84f5a59ac530e02c7a3de53b86873e9312a
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-03-09 11:25:03 +0200

    Time: 197 ms (5.09%), Space: 109.8 MB (5.15%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<long long,int> fre1, fre2;
+        for(auto &i : nums1){
+            if(!fre1[i]) fre1[i]++;
+        }
+        for(auto &i : nums2){
+            if(!fre2[i]) fre2[i]++;
+        }
+        long long ans = 2e9;
+        for(auto &[l, r] : fre1){
+            if(fre2[l]) ans = min(ans, l);
+        }
+        if(ans == 2e9) ans = -1;
+        return ans;
+    }
+};
