61f567f9eaeab09e1db1d42cc864a8a225bd1209
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-04-02 15:07:13 +0200

    Time: 6 ms (65.28%), Space: 8.1 MB (94.60%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<int,int> idxs, idxt;
+        vector<int>idxs(200),idxt(200);
         for(int i = 0;i < s.size();++i){
-            if(idxs[s[i]-'a'] != idxt[t[i]-'a']) return false;
-            idxs[s[i]-'a'] = i + 1;
-            idxt[t[i]-'a'] = i + 1;
+            if(idxs[s[i]] != idxt[t[i]]) return false;
+            idxs[s[i]] = i + 1;
+            idxt[t[i]] = i + 1;
         }
         return true;
     }
