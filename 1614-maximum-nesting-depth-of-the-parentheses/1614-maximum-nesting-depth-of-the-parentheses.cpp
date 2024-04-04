669791692905b39aabf8a64ea59fd2838664d7f7
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-04-04 14:46:23 +0200

    Time: 0 ms (100.00%), Space: 7.2 MB (96.44%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int L = 0, R = 0, ans = 0;
+        for(int i = 0;i < s.size();++i){
+            if(s[i] == '(') ++L, ans = max(ans, L);
+            else if(s[i] == ')'){
+                --L;
+            }else if(i + 1 < s.size() && s[i + 1] == ')'){
+                ans = max(ans, L);
+            }
+        }
+        return ans ;
+    }
+};
