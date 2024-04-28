ece8ba8a930c4436aab752588bd6fd7db3ec8730
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-04-29 01:05:41 +0300

    Time: 12 ms (79.16%), Space: 15.6 MB (60.13%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int findRotateSteps(string ring, string key) {
         vector<vector<int>>dp(key.size(), vector<int>(ring.size(), -1));
-        return best(0, 0, ring, key, dp);
+        vector<vector<int>>adj(26);
+        for(int i = 0;i < ring.size();++i){
+            adj[ring[i] - 'a'].push_back(i);
+        }
+        return best(0, 0, ring, key, dp, adj);
     }
-    int best(int i, int lst,string &ring, string &key, vector<vector<int>>&dp){
+    int best(int i, int lst,string &ring, string &key, vector<vector<int>>&dp,vector<vector<int>>&adj){
         if(i == key.size()) return key.size();
         int &ret = dp[i][lst];
         if(~ret) return ret ;
         ret = 1e9;
-        for(int j = 0;j < ring.size();++j){
+        for(auto &j : adj[key[i] - 'a']){
             if(ring[j] == key[i]){
                 int add = abs(lst - j);
                 if(j < lst){
                     add = min(add, (int)ring.size() - lst + j);
                 }
                 add = min(add, (int)ring.size() - j + lst);
-                ret = min(ret, best(i + 1, j, ring, key, dp) + add);
+                ret = min(ret, best(i + 1, j, ring, key, dp, adj) + add);
             }
         }
         return ret ;
