ce91e2825542af8294c3f5ac3f81744cf641ed4b
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-04-29 03:15:23 +0300

    Time: 134 ms (25.83%), Space: 91.4 MB (25.13%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int ans = 0 , n = nums.size();
-        for(int i = 0;i < 25;++i){
+        for(int i = 0;i < 20;++i){
             int k_bit = ((k >> i) & 1);
             int Xor = 0;
             for(auto &j : nums){
