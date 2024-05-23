3f67b77c5e5271e56ab4df8a4b72cc4c29251aad
Eslam Abdelwahid <95712091+EslamAbdelwahid@users.noreply.github.com>
2024-05-24 02:41:15 +0300

    Time: 1022 ms (5.09%), Space: 393.4 MB (5.09%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
+        vector<vector<int>> dp(nums.size(), vector<int>(1 << nums.size(), -1));
         function<int(int,int)> calc = [&](int i, int Mask){
             bitset < 20 > mask = Mask;
             if(i == nums.size()) return 1;
-            int ret = calc(i + 1, Mask);
+            int &ret = dp[i][Mask];
+            if(~ret) return ret;
+            ret = calc(i + 1, Mask);
             for(int j = 0;j < i;++j){
                 if(mask[j] && abs(nums[i] - nums[j]) == k){
                     return ret ;
